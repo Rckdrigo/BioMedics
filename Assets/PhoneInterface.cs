c4086868b1196ebba42c7abe34af828d0709d13c
Rckdrigo <rckdrigomed@gmail.com>
2014-01-27 15:21:56 -0600

    phone interface

---
+++ Assets/PhoneInterface.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(NetworkView))]
+public class PhoneInterface : MonoBehaviour {
+
+	public static Vector3 acceleration, gyroscope;
+
+	void Update(){
+		if(Network.isClient)
+			networkView.RPC("sendVector",RPCMode.Server,Input.acceleration,Input.gyro.rotationRate);
+		//acc = new Vector3(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"),0);
+		//gyro = new Vector3(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"),0);
+
+	}
+
+	[RPC]
+	void sendVector(Vector3 acc, Vector3 gyro){
+		acceleration = acc;
+		gyroscope = gyro;
+	}
+
+	void OnGUI(){
+		print(Network.peerType);
+		GUILayout.Space(50);
+
+		GUILayout.Label("Acceleration: "+acceleration + " Gyro: " + gyroscope);
+	}
+}
